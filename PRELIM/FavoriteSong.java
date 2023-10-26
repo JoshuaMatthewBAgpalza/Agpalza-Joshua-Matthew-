ace32bf3bdf9a0c3a7720e97d593f31a83c20cae
JoshuaMatthewBAgpalza <148180885+JoshuaMatthewBAgpalza@users.noreply.github.com>
2023-10-26 05:58:52 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class FavoriteSong{
+
+public static void main(String[] args){
+
+            System.out.println("I used to roll the dice");
+            System.out.println("Listen as the crowd would sing");
+				System.out.println("Now the old king is dead, long live the king");
+				System.out.println("One minute, I held the key");
+				System.out.println("Next the walls were closed on me");
+            
+        	}
+ }
+ 
+ 
